465725c43235462286300654277a57454badec03
Anurag Raju <anuragraju.480@gmail.com>
2023-11-30 07:33:37 +0530

    Added Client and Server folders

---
+++ Server/food-gateway/food-gateway/src/main/java/com/gateway/foodgateway/FoodGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.foodgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodGatewayApplication.class, args);
+	}
+
+}
---
+++ Server/food-gateway/food-gateway/src/test/java/com/gateway/foodgateway/FoodGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.foodgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
